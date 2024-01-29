994864fd60cbd24d22bd3962fd33b01448647fd1
sanganiviraj <sanganiviraj263@gmail.com>
2024-01-29 11:05:01 +0530

    successful video call

--- Src/Helpers/Videocall.js
+++ Src/Helpers/Videocall.js
@@ -1,30 +1,59 @@
-// App.js
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions, StatusBar } from 'react-native';
-import ZegoUIKitPrebuiltCall , { ONE_ON_ONE_VIDEO_CALL_CONFIG } from '@zegocloud/zego-uikit-prebuilt-call-rn';
+import AgoraUIKit from 'agora-rn-uikit';
+import React, { Component, useEffect, useState } from 'react';
+import { PermissionsAndroid, StyleSheet, Text, View  } from 'react-native';
+import RNCallKeep from 'react-native-callkeep';
 
-export default function VoiceCallPage({route}) {
-    randomuserid = String(Math.floor(Math.random()*100000))
-    // 
-    // const {nm} = route.params;
-    // console.log('route =>',nm);
+
+export default function VoiceCallPage() {
+    const [videoCall, setVideoCall] = useState(true);
+
+    const connectionData = {
+        appId: 'e2f11f0a061f41158a32e5b4b461b60a',
+        channel: 'logicwave',
+        token: '007eJxTYLiu+z8yVyb44QHjuQFb4t2uiTlqOCZJtx7+vneuTtWycAcFhlSjNEPDNINEAzPDNBNDQ1OLRGOjVNMkkyQTM8MkM4PEp1O3pDYEMjLsWWHEzMgAgSA+J0NOfnpmcnliWSoDAwDDqyEy', // enter your channel token as a string 
+    };
+
+    const rtcCallbacks = {
+        EndCall: () => setVideoCall(false),
+    };
+
+  
+
+     
+
+      useEffect(()=>{
+        RNCallKeep.setup({
+            ios: {
+              appName: 'CallKeepDemo',
+            },
+            android: {
+               alertTitle: 'Permissions required',
+              alertDescription: 'This application needs to access your phone accounts',
+              cancelButton: 'Cancel',
+              okButton: 'ok',
+            //   additionalPermissions: [PermissionsAndroid.BIND_TELECOM_CONNECTION_SERVICE],
+            //   // Required to get audio in background when using Android 11
+            //   foregroundService: {
+            //     channelId: 'com.company.my',
+            //     channelName: 'Foreground service for my app',
+            //     notificationTitle: 'My app is running on background',
+            //     notificationIcon: 'Path to the resource icon of the notification',
+            //   }, 
+            },
+          });
+
+        RNCallKeep.startCall("uuid", "12332432432", "vs");
+      },[])
 
     return (
         <View style={styles.container}>
-            <ZegoUIKitPrebuiltCall
-                appID={645773714}
-                appSign={"64bdea2e05ef9cf611a5b452df4f24907858da24854030f7b17cd91052c8c7be"}
-                userID={randomuserid} // userID can be something like a phone number or the user id on your own user system. 
-                userName={'use_'+randomuserid}
-                callID={"testcallid"} // callID can be any unique string. 
 
-                config={{
-                    // You can also use ONE_ON_ONE_VOICE_CALL_CONFIG/GROUP_VIDEO_CALL_CONFIG/GROUP_VOICE_CALL_CONFIG to make more types of calls.
-                    ...ONE_ON_ONE_VIDEO_CALL_CONFIG,
-                    onOnlySelfInRoom: () => { props.navigation.navigate('HomePage') },
-                    onHangUp: () => { props.navigation.navigate('HomePage') },
-                }}
-            />
+           { videoCall ? (
+                    <AgoraUIKit connectionData={connectionData} rtcCallbacks={rtcCallbacks} />
+                ) : (
+                    <Text onPress={()=>setVideoCall(true)}>Start Call</Text>
+                )
+            }
         </View>
     );
 }
--- Src/Screens/Chatscreen.js
+++ Src/Screens/Chatscreen.js
@@ -1,7 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import getAuth from '@react-native-firebase/auth'
 
 const Chatscreen = () => {
+  const [datas,setdatas] = useState();
+
+  useEffect(() => {
+    getAuth().onAuthStateChanged((currentuser) => {
+      console.log("currentuser => " , currentuser);
+     })
+  },[])   
+
   return (
     <View>
       <Text>Chatscreen</Text>
