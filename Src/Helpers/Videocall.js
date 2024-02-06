f490476f8d655630f5f5f43ef0510626a302bedb
sanganiviraj <sanganiviraj263@gmail.com>
2024-02-06 18:19:02 +0530

    ok

--- Src/Helpers/Videocall.js
+++ Src/Helpers/Videocall.js
@@ -1,7 +1,6 @@
 import AgoraUIKit from 'agora-rn-uikit';
 import React, { Component, useEffect, useState } from 'react';
 import { PermissionsAndroid, StyleSheet, Text, View  } from 'react-native';
-import RNCallKeep from 'react-native-callkeep';
 
 
 export default function VoiceCallPage() {
@@ -21,29 +20,29 @@ export default function VoiceCallPage() {
 
      
 
-      useEffect(()=>{
-        RNCallKeep.setup({
-            ios: {
-              appName: 'CallKeepDemo',
-            },
-            android: {
-               alertTitle: 'Permissions required',
-              alertDescription: 'This application needs to access your phone accounts',
-              cancelButton: 'Cancel',
-              okButton: 'ok',
-            //   additionalPermissions: [PermissionsAndroid.BIND_TELECOM_CONNECTION_SERVICE],
-            //   // Required to get audio in background when using Android 11
-            //   foregroundService: {
-            //     channelId: 'com.company.my',
-            //     channelName: 'Foreground service for my app',
-            //     notificationTitle: 'My app is running on background',
-            //     notificationIcon: 'Path to the resource icon of the notification',
-            //   }, 
-            },
-          });
+      // useEffect(()=>{
+      //   RNCallKeep.setup({
+      //       ios: {
+      //         appName: 'CallKeepDemo',
+      //       },
+      //       android: {
+      //          alertTitle: 'Permissions required',
+      //         alertDescription: 'This application needs to access your phone accounts',
+      //         cancelButton: 'Cancel',
+      //         okButton: 'ok',
+      //       //   additionalPermissions: [PermissionsAndroid.BIND_TELECOM_CONNECTION_SERVICE],
+      //       //   // Required to get audio in background when using Android 11
+      //       //   foregroundService: {
+      //       //     channelId: 'com.company.my',
+      //       //     channelName: 'Foreground service for my app',
+      //       //     notificationTitle: 'My app is running on background',
+      //       //     notificationIcon: 'Path to the resource icon of the notification',
+      //       //   }, 
+      //       },
+      //     });
 
-        RNCallKeep.startCall("uuid", "12332432432", "vs");
-      },[])
+      //   RNCallKeep.startCall("uuid", "12332432432", "vs");
+      // },[])
 
     return (
         <View style={styles.container}>
